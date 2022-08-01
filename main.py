dc6f63d6bd2a4c27c19756848fcff2106bd4ef51
Kim hyeong woo <cowofbrother@gmail.com>
2022-08-01 21:47:17 +0900

    v6

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ view = [100, 78, 4, 30]
 
 st.write('# View List')
 
-st.write('## raw data')
-view
+show = st.checkbox('show raw data')
+if show == True:
+    st.write('## raw data')
+    view
 
 st.write('## bar chart')
 st.bar_chart(view)
